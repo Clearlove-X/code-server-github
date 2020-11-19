7eafff4fb8ce8e0a0d997552af6e53ccd73aedc5
王玉堂 <wangyutang@inspur.com>
2020-11-19 20:44:41 +0800

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -163,7 +163,7 @@ class Builder {
 		} else {
 			await this.task("Cloning VS Code", () => {
 				return util.promisify(cp.exec)(
-					"git clone https://github.com/yuxiguang/vscode"
+					"git clone http://oauth2:xqujyVvh3nY1zRdPU4vE@git.inspur.com/cloudide/vscode.git"
 						+ ` --quiet --branch "${vscodeVersion}"`
 						+ ` --single-branch --depth=1 "${vscodeSourcePath}"`);
 			});
